7d3377aef9964bb16bd526421a489265e4af1364
Vladimir Panteleev <vladimir@thecybershadow.net>
2011-11-21 22:47:11 +0200

    Group index

--- web.d
+++ web.d
@@ -39,8 +39,11 @@ class WebUI
 
 	HttpResponse onRequest(HttpRequest request, ClientSocket from)
 	{
-		log(format("%s - %s", from.remoteAddress, request.resource));
+		StopWatch responseTime;
+		responseTime.start();
+		scope(exit) log(format("%s - %dms - %s", from.remoteAddress, responseTime.peek().msecs, request.resource));
 		auto response = new HttpResponseEx();
+		string title, content;
 		try
 		{
 			auto pathStr = request.resource;
@@ -64,16 +67,26 @@ class WebUI
 					switch (path[1])
 					{
 						case "":
-							discussionIndex(response);
-							return response;
+							title = "Index";
+							content = discussionIndex();
+							break;
+						case "group":
+							enforce(path.length >= 2, "No group specified");
+							title = path[2] ~ " index";
+							content = discussionGroup(path[2], to!int(aaGet(parameters, "page", "1")));
+							break;
 						default:
 							return response.writeError(HttpStatusCode.NotFound);
 					}
+					break;
 				}
 				default:
 					//return response.writeError(HttpStatusCode.NotFound);
 					return response.serveFile(pathStr[1..$], "web/static/");
 			}
+
+			assert(title && content);
+			return response.serveData(HttpResponseEx.loadTemplate("web/skel.htt", ["title" : title, "content" : content]));
 		}
 		catch (Exception e)
 			return response.writeError(HttpStatusCode.InternalServerError, "Unprocessed exception: " ~ e.msg);
@@ -127,12 +140,13 @@ class WebUI
 		{ "D",	"Retired, use digitalmars.D instead" },
 	]}];
 
-	void discussionIndex(HttpResponseEx response)
+	string discussionIndex()
 	{
 		int[string] threadCounts;
 		int[string] postCounts;
 		string[string] lastPosts;
 
+		// TODO: the next two queries take about 300 ms in total, cache this data
 		foreach (string group, int count; query("SELECT `Group`, COUNT(*) FROM `Threads` GROUP BY `Group`").iterate())
 			threadCounts[group] = count;
 		foreach (string group, int count; query("SELECT `Group`, COUNT(*) FROM `Groups`  GROUP BY `Group`").iterate())
@@ -142,43 +156,109 @@ class WebUI
 				foreach (string id; query("SELECT `ID` FROM `Groups` WHERE `Group`=? ORDER BY `Time` DESC LIMIT 1").iterate(group.name))
 					lastPosts[group.name] = id;
 
-		string index =
-			`<table id="forum-index">` ~
+		string summarizePost(string postID)
+		{
+			auto info = getPostInfo(postID);
+			if (info)
+				with (*info)
+					return
+						`<a class="forum-postsummary-subject" href="/discussion/post/` ~ encodeEntities(id[1..$-1]) ~ `">` ~ truncateString(subject) ~ `</a><br>` ~
+						`by <span class="forum-postsummary-author">` ~ truncateString(author) ~ `</span><br>` ~
+						`<span class="forum-postsummary-time">` ~ summarizeTime(time) ~ `</span>`;
+
+			return `<div class="forum-no-data">-</div>`;
+		}
+
+		return
+			`<table id="forum-index" class="forum-table">` ~
 			join(array(map!(
 				(GroupSet set) { return
-					`<tr class="forum-index-set-header"><th colspan="4">` ~ set.name ~ `</th></tr>` ~ newline ~
+					`<tr class="forum-index-set-header"><th colspan="4">` ~ encodeEntities(set.name) ~ `</th></tr>` ~ newline ~
 					`<tr class="forum-index-set-captions"><th>Forum</th><th>Last Post</th><th>Threads</th><th>Posts</th>` ~ newline ~
 					join(array(map!(
 						(Group group) { return `<tr>` ~
-							`<td class="forum-index-col-forum"><a href="/discussion/` ~ encodeEntities(group.name) ~ `/">` ~ encodeEntities(group.name) ~ `</a>` ~
+							`<td class="forum-index-col-forum"><a href="/discussion/group/` ~ encodeEntities(group.name) ~ `">` ~ encodeEntities(group.name) ~ `</a>` ~
 								`<div class="forum-index-description">` ~ encodeEntities(group.description) ~ `</div>` ~
 							`</td>` ~
 							`<td class="forum-index-col-lastpost">`    ~ (group.name in lastPosts    ? summarizePost(lastPosts[group.name]) : `<div class="forum-no-data">-</div>`) ~ `</td>` ~
-							`<td class="forum-index-col-threadcount">` ~ (group.name in threadCounts ? text(threadCounts[group.name]) : `-`) ~ `</td>` ~
-							`<td class="forum-index-col-postcount">`   ~ (group.name in postCounts   ? text(postCounts[group.name]) : `-`)  ~ `</td>` ~
+							`<td class="forum-index-col-threadcount">` ~ (group.name in threadCounts ? formatNumber(threadCounts[group.name]) : `-`) ~ `</td>` ~
+							`<td class="forum-index-col-postcount">`   ~ (group.name in postCounts   ? formatNumber(postCounts[group.name]) : `-`)  ~ `</td>` ~
 							`</tr>` ~ newline;
 						}
 					)(set.groups)));
 				}
 			)(groupHierarchy))) ~
 			`</table>`;
-
-		response.serveData(HttpResponseEx.loadTemplate("web/skel.htt", ["content" : index]));
 	}
 
-	string summarizePost(string id)
+	string discussionGroup(string group, int page)
+	{
+		enum THREADS_PER_PAGE = 25;
+
+		page--;
+		enforce(page >= 0, "Invalid page");
+
+		struct Thread
+		{
+			PostInfo* _firstPost, _lastPost;
+			int postCount;
+
+			/// Handle orphan posts
+			@property PostInfo* thread() { return _firstPost ? _firstPost : _lastPost; }
+			@property PostInfo* lastPost() { return _lastPost ? _lastPost : _firstPost; }
+		}
+		Thread[] threads;
+
+		foreach (string firstPostID, string lastPostID; query("SELECT `ID`, `LastPost` FROM `Threads` WHERE `Group` = ? ORDER BY `LastUpdated` DESC LIMIT ? OFFSET ?").iterate(group, THREADS_PER_PAGE, page*THREADS_PER_PAGE))
+			foreach (int count; query("SELECT COUNT(*) FROM `Posts` WHERE `ThreadID` = ?").iterate(firstPostID))
+				threads ~= Thread(getPostInfo(firstPostID), firstPostID==lastPostID ? null : getPostInfo(lastPostID), count);
+
+		string summarizeThread(PostInfo* info)
+		{
+			assert(info !is null);
+			with (*info)
+				return
+					`<a class="forum-postsummary-subject" href="/discussion/post/` ~ encodeEntities(id[1..$-1]) ~ `">` ~ truncateString(subject, 100) ~ `</a><br>` ~
+					`by <span class="forum-postsummary-author">` ~ truncateString(author, 100) ~ `</span><br>`;
+		}
+
+		string summarizeLastPost(PostInfo* info)
+		{
+			if (info)
+				with (*info)
+					return
+						`<span class="forum-postsummary-time">` ~ summarizeTime(time) ~ `</span>` ~
+						`by <span class="forum-postsummary-author">` ~ truncateString(author) ~ `</span><br>`;
+
+			return `<div class="forum-no-data">-</div>`;
+		}
+
+		return
+			`<table id="group-index" class="forum-table">` ~
+			`<tr class="forum-index-set-header"><th colspan="3">` ~ encodeEntities(group) ~ `</th></tr>` ~ newline ~
+			`<tr class="forum-index-set-captions"><th>Thread / Thread Starter</th><th>Last Post</th><th>Replies</th>` ~ newline ~
+			join(array(map!(
+				(Thread thread) { return `<tr>` ~
+					`<td class="group-index-col-first">` ~ summarizeThread(thread.thread) ~ `</td>` ~
+					`<td class="group-index-col-last">`  ~ summarizeLastPost(thread.lastPost) ~ `</td>` ~
+					`<td class="group-index-col-replies">`  ~ formatNumber(thread.postCount-1) ~ `</td>` ~
+					`</tr>` ~ newline;
+				}
+			)(threads))) ~
+			`</table>`;
+	}
+
+	struct PostInfo { string id, author, subject; SysTime time; }
+
+	PostInfo* getPostInfo(string id)
 	{
 		if (id.startsWith('<') && id.endsWith('>'))
 			foreach (string author, string subject, long stdTime; query("SELECT `Author`, `Subject`, `Time` FROM `Posts` WHERE `ID` = ?").iterate(id))
-				return
-					`<a class="forum-postsummary-subject" href="/discussion/post/` ~ encodeEntities(id[1..$-1]) ~ `">` ~ truncateString(subject) ~ `</a><br>` ~
-					`by <span class="forum-postsummary-author">` ~ truncateString(author) ~ `</span><br>` ~
-					`<span class="forum-postsummary-time">` ~ summarizeTime(stdTime) ~ `</span>`;
-
-		return "-";
+				return [PostInfo(id, author, subject, SysTime(stdTime, UTC()))].ptr;
+		return null;
 	}
 
-	string summarizeTime(long stdTime)
+	string summarizeTime(SysTime time)
 	{
 		string ago(long amount, string units)
 		{
@@ -186,7 +266,6 @@ class WebUI
 			return format("%s %s%s ago", amount, units, amount==1 ? "" : "s");
 		}
 
-		auto time = SysTime(stdTime, UTC());
 		auto now = Clock.currTime();
 		scope(failure) std.stdio.writeln([time, now]);
 		auto duration = now - time;
@@ -218,6 +297,17 @@ class WebUI
 			//shortTime = time.toSimpleString();
 
 		return `<span title="` ~ encodeEntities(formatTime("l, d F Y, H:i:s e", time)) ~ `">` ~ shortTime ~ `</span>`;
+	}
+
+	/// Add thousand-separators
+	string formatNumber(long n)
+	{
+		string s = text(n);
+		int digits = 0;
+		foreach_reverse(p; 0..s.length-1)
+			if (++digits % 3 == 0)
+				s = s[0..p] ~ ',' ~ s[p..$];
+		return s;
 	}
 
 	string truncateString(string s, int maxLength = 30)
