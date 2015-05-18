47c0d721a945dc77c0ed7edf27598c103e9183c1
Vladimir Panteleev <vladimir@thecybershadow.net>
2015-05-18 01:01:34 +0000

    cache: Add debug=NOCACHE

--- cache.d
+++ cache.d
@@ -1,4 +1,4 @@
-/*  Copyright (C) 2011, 2012, 2014  Vladimir Panteleev <vladimir@thecybershadow.net>
+/*  Copyright (C) 2011, 2012, 2014, 2015  Vladimir Panteleev <vladimir@thecybershadow.net>
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU Affero General Public License as
@@ -42,6 +42,7 @@ struct Cached(T)
 		{
 			cachedData = dataSource;
 			cacheVersion = dbVersion;
+			debug(NOCACHE) cacheVersion = -1;
 		}
 		return cachedData;
 	}
