cbcffc332bb954d70130cd194eb2b00d5801e48c
Vladimir Panteleev <git@thecybershadow.net>
2018-03-17 23:54:36 +0000

    dfeed.web.web: Swap "Send" and "Save and Preview" buttons
    Fixes issue #101.

--- src/dfeed/web/web.d
+++ src/dfeed/web/web.d
@@ -2633,8 +2633,8 @@ bool discussionPostForm(PostDraft draft, bool showCaptcha=false, PostError error
 	html.put(
 		`<div>` ~
 			`<div class="postform-action-left">` ~
-				`<input name="action-send" type="submit" value="Send">` ~
 				`<input name="action-save" type="submit" value="Save and preview">` ~
+				`<input name="action-send" type="submit" value="Send">` ~
 			`</div>` ~
 			`<div class="postform-action-right">` ~
 				`<input name="action-discard" type="submit" value="Discard draft">` ~
