CyberShadow/DFeed

commit b2168b47ee76d774daeb60f8ca63c887ab7aacd9

dfeed.web.web: Move the <pre> block into formatBody
This allows us to attach classes to it depending on the body we will
emit.

src/dfeed/web/web/part/postbody.d
@@ -36,6 +36,7 @@ enum reURL = `\w+://[^<>\s]+[\w/\-+=]`;
 
 void formatBody(Rfc850Message post)
 {
+	html.put(`<pre class="post-text">`);
 	auto paragraphs = unwrapText(post.content, post.wrapFormat);
 	bool inSignature = false;
 	int quoteLevel = 0;
@@ -178,6 +179,7 @@ void formatBody(Rfc850Message post)
 		html ~= `</span>`;
 	if (inSignature)
 		html ~= `</span>`;
+	html.put(`</pre>`);
 }
 
 // https://github.com/CyberShadow/DFeed/issues/121
@@ -191,5 +193,5 @@ http://a/b+
 EOF");
 	scope(exit) html.clear();
 	formatBody(msg);
-	assert(html.get.strip == `<a rel="nofollow" href="http://a/b+">http://a/b+</a>`);
+	assert(html.get.strip == `<pre class="post-text"><a rel="nofollow" href="http://a/b+">http://a/b+</a></pre>`);
 }

src/dfeed/web/web/part/post.d
@@ -1,4 +1,4 @@
-﻿/*  Copyright (C) 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2020  Vladimir Panteleev <vladimir@thecybershadow.net>
+﻿/*  Copyright (C) 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2020, 2021  Vladimir Panteleev <vladimir@thecybershadow.net>
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU Affero General Public License as
@@ -253,9 +253,9 @@ void formatPost(Rfc850Post post, Rfc850Post[string] knownPosts, bool markAsRead 
 		html.put(
 					`<div class="post-actions">`), postActions(actions), html.put(`</div>` ~
 				`</td>` ~
-				`<td class="post-body">` ~
+				`<td class="post-body">`,
 //		); miniPostInfo(post, knownPosts); html.put(
-					`<pre class="post-text">`), formatBody(post), html.put(`</pre>`,
+					), formatBody(post), html.put(
 					(error ? `<span class="post-error">` ~ encodeHtmlEntities(error) ~ `</span>` : ``),
 				`</td>` ~
 			`</tr>` ~

src/dfeed/web/web/view/feed.d
@@ -1,4 +1,4 @@
-﻿/*  Copyright (C) 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2020  Vladimir Panteleev <vladimir@thecybershadow.net>
+﻿/*  Copyright (C) 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2020, 2021  Vladimir Panteleev <vladimir@thecybershadow.net>
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU Affero General Public License as
@@ -101,9 +101,7 @@ CachedResource makeFeed(Rfc850Post[] posts, string feedUrl, string feedTitle, bo
 	foreach (post; posts)
 	{
 		html.clear();
-		html.put("<pre>");
 		formatBody(post);
-		html.put("</pre>");
 
 		auto postTitle = post.rawSubject;
 		if (addGroup)

src/dfeed/web/web/view/post.d
@@ -1,4 +1,4 @@
-﻿/*  Copyright (C) 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2020, 2020  Vladimir Panteleev <vladimir@thecybershadow.net>
+﻿/*  Copyright (C) 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2020, 2020, 2021  Vladimir Panteleev <vladimir@thecybershadow.net>
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU Affero General Public License as
@@ -118,8 +118,8 @@ void formatSplitPost(Rfc850Post post, bool footerNav)
 		miniPostInfo(post, null);
 		html.put(
 				`</td></tr>` ~
-				`<tr class="post-layout-body"><td>` ~
-					`<pre class="post-text">`), formatBody(post), html.put(`</pre>`,
+				`<tr class="post-layout-body"><td>`,
+					), formatBody(post), html.put(
 					(error ? `<span class="post-error">` ~ encodeHtmlEntities(error) ~ `</span>` : ``),
 				`</td></tr>` ~
 				`<tr class="post-layout-footer"><td>`

commit baf001a79f2161b3c67ed7c53bea0ee5ceb13746

dfeed.web.web.part.postbody: Update unittest
Broken in b2168b47ee76d774daeb60f8ca63c887ab7aacd9.
Same commit did update the unit test, but not sufficiently.

src/dfeed/web/web/part/postbody.d
@@ -222,5 +222,7 @@ http://a/b+
 EOF");
 	scope(exit) html.clear();
 	formatBody(msg);
-	assert(html.get.strip == `<pre class="post-text"><a rel="nofollow" href="http://a/b+">http://a/b+</a></pre>`);
+	assert(html.get.strip == `<pre class="post-text"><a rel="nofollow" href="http://a/b+">http://a/b+</a>
+
+</pre>`, html.get.strip);
 }