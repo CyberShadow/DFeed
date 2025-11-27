CyberShadow/DFeed

commit 8778b1430afb4c4934c7e3d7af536abc6db89a2b

dfeed.web: Quarantine messages after CAPTCHA challenge
If a post has qualities (e.g. extremely high spamicity) which would
warrant quarantining it for moderation, make that decision after the
user solved any CAPTCHA challenges, rather than before. This prevents
obvious spam from simple spam bots going to moderators' inboxes.

src/dfeed/web/web/posting.d
@@ -349,16 +349,6 @@ string discussionSend(UrlParameters clientVars, Headers headers)
 					}
 				}
 
-				auto moderationReason = shouldModerate(draft);
-				if (moderationReason.kind != ModerationReason.Kind.none)
-				{
-					// draft will be saved by scope(exit) above
-					moderateMessage(draft, headers, moderationReason);
-
-					html.put(`<p>`, _!`Your message has been saved, and will be posted after being approved by a moderator.`, `</p>`);
-					return null;
-				}
-
 				auto pid = postDraft(draft, headers);
 
 				lastPostAttempts[ip] ~= Clock.currTime();
@@ -568,6 +558,10 @@ void discussionPostStatus(PostProcess process, out bool refresh, out string redi
 			redirectTo = idToUrl(process.post.id);
 			discussionPostStatusMessage(_!`Message posted! Redirecting...`);
 			refresh = true;
+			return;
+
+		case PostingStatus.moderated:
+			discussionPostStatusMessage(_!`Your message has been saved, and will be posted after being approved by a moderator.`);
 			return;
 
 		case PostingStatus.captchaFailed:

src/dfeed/web/posting.d
@@ -43,6 +43,8 @@ import dfeed.sources.newsgroups : NntpConfig;
 import dfeed.web.captcha;
 import dfeed.web.spam;
 import dfeed.web.user;
+import dfeed.web.web.postmod : ModerationReason, shouldModerate;
+import dfeed.web.web.posting : moderateMessage;
 
 struct PostDraft
 {
@@ -84,6 +86,7 @@ enum PostingStatus
 	posting,
 	waiting,
 	posted,
+	moderated,
 
 	captchaFailed,
 	spamCheckFailed,
@@ -369,7 +372,7 @@ private:
 			log("  (user solved %d CAPTCHAs)".format(n));
 		}
 
-		postMessage();
+		checkForModeration();
 	}
 
 	void onSpamResult(Spamicity spamicity, string errorMessage)
@@ -383,6 +386,21 @@ private:
 			return;
 		}
 		log("Spam check OK (spamicity: %.2f)".format(spamicity));
+
+		checkForModeration();
+	}
+
+	void checkForModeration()
+	{
+		auto moderationReason = shouldModerate(draft);
+		if (moderationReason.kind != ModerationReason.Kind.none)
+		{
+			this.status = PostingStatus.moderated;
+			moderateMessage(draft, headers, moderationReason);
+			log("Quarantined for moderation: " ~ moderationReason.toString());
+			log.close();
+			return;
+		}
 
 		postMessage();
 	}

commit d7c5bbcfd88c471cc01e00c1c307ee99a9f8a3b5

dfeed.web.web.posting: Fix messages not being saved to moderation queue
Bug introduced in 8778b1430afb4c4934c7e3d7af536abc6db89a2b.

src/dfeed/web/web/posting.d
@@ -27,6 +27,7 @@ import std.datetime.timezone : UTC;
 import std.exception : enforce;
 import std.format : format;
 import std.string : strip, splitLines;
+import std.typecons : Yes;
 
 import ae.net.ietf.headers : Headers;
 import ae.net.ietf.url : UrlParameters, encodeUrlParameter;
@@ -404,6 +405,7 @@ void moderateMessage(ref PostDraft draft, Headers headers, ModerationReason reas
 	learnModeratedMessage(draft, true, 1);
 	draft.serverVars["headers"] = headers.to!(string[][string]).toJson;
 	draft.status = PostDraft.Status.moderation;
+	saveDraft(draft, Yes.force);
 
 	string sanitize(string s) { return "%(%s%)".format(s.only)[1..$-1]; }
 